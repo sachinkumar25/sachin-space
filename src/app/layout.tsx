2b7fff592f6992c63e4ea15fd126b9d1d2528a9c
Sachin Satish Kumar <sachinkumar@Sachins-MacBook-Pro.local>
2026-01-26 15:08:11 -0500

    Add suppressHydrationWarning to body to ignore extension attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
